d98432901f7bd389e820c9d8eeac595821233c13
@IDI <idelaiglesia.m@gmail.com>
2025-01-22 17:43:06 +0100

    filler for lesson end, more tomorrow

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 "use strict";
-const students = [
-    {
-        name: "John",
-        score: 70,
-    },
-    {
-        name: "Mark",
-        score: 90,
-    }
-];
-for (const student of students) {
-    console.log(`${student.name} scored ${student.grade}`); // Typescript will showcase errors or mistakes since it "stores" the type. You can also view further info hovering and picking view problem.
+function addNumbers(a, b) {
+    return a + b;
 }
+console.log(addNumbers(2, 3));
+// console.log(addNumbers(2, "tres"));
--- index.ts
+++ index.ts
@@ -1,14 +1,6 @@
-const students = [
-    {
-        name: "John",
-        score: 70,
-    },
-    {
-        name: "Mark",
-        score: 90,
-    }
-];
-
-for(const student of students) {
-    console.log(`${student.name} scored ${student.grade}`); // Typescript will showcase errors or mistakes since it "stores" the type. You can also view further info hovering and picking view problem.
+function addNumbers (a: number, b: number) {
+    return a + b
 }
+
+console.log(addNumbers(2, 3));
+// console.log(addNumbers(2, "tres"));
