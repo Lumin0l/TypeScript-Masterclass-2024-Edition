b4bb1773d57bfb2f44469c6a2bbc0ebb4a72ce99
Imanol <idelaiglesia.m@gmail.com>
2025-01-26 12:26:24 +0100

    section3. exercise check

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 let city = "New York"; // string
 let population = 8400000; // number
-const age = 32; // number
-let oldAge = 79 as const; // const
+const age = 32; //! number -> it is literal 32 because it's a const and thus fixed.
+let oldAge = 79 as const; //! const -> yes and no it's literal 79
 let newAge = oldAge; // const number 79
 let data = new Map(); // Map
-let score = [90, 86, 100]; // array<any>
+let score = [90, 86, 100]; //! array<any> -> number[]
 type Primitive = string | number | boolean; // string or number or boolean, since it's a union type
 type CustomName = "John" extends string ? string : "John"; // string
 type CustomAge = typeof newAge extends number ? 79 : number; // 79
@@ -16,9 +16,9 @@ type CheckScore = typeof score extends never ? {} : []; // []
  * Check if below lines of code are valid as per TypeScript or not without uncommenting them
  *  */
 
-// age = 85; -> yes
+// age = 85; -> yes !-> it is a const, can't be reassigned.
 // score.push(10); -> yes
-// score.push("New Score"); -> yes
-// let customAge: CustomAge = 50; -> yes
-// let primitive: Primitive = new Date(); -> yes
+// score.push("New Score"); -> yes !-> error, because it is a number array. 
+// let customAge: CustomAge = 50; -> yes !-> no, because it is a const literal of 79
+// let primitive: Primitive = new Date(); -> yes !-> no, because it's cast as string 
 // let years: CheckScore = []; -> yes
