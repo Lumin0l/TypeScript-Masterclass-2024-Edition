b7192ed66c6e4d8d6c642af96d63318315521d51
Imanol <idelaiglesia.m@gmail.com>
2025-01-26 09:34:04 +0100

    26: understanding type hierarchy

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 "use strict";
-// We'll dive deeper into Conditional Types later in the course
+// A funtion declaration that never completes and returns a never type
+const throwError = (errorMsg) => {
+    throw new Error(errorMsg);
+};
+// If we use the parent type for an annotation, like object to annotate an array, TS won't complain because object is the parent
+let strings = ["a", "b"];
+let myFunc = () => 2;
--- index.ts
+++ index.ts
@@ -1,9 +1,17 @@
-type CustomDate = Date;
-type CustomString = string;
+// A subtype will always exten the parent type
+type check = any extends unknown ? true : false;
+type check2 = string extends any ? true : false;
+type check3 = number extends any ? true : false;
+type check4 = {} extends Object ? true : false;
+type check5 = [] extends Object ? true : false;
+type check6 = Function extends Object ? true : false;
+type check7 = never extends any ? true : false;
 
-// Type declarations using conditional types in TypeScript
-type TrueString = CustomString extends string ? true : false;
-type ConditionalNumber = CustomDate extends Date ? number : string;
-type DateAssigment = CustomDate extends Date ? Date : undefined;
+// A funtion declaration that never completes and returns a never type
+const throwError = (errorMsg: string) => {
+  throw new Error(errorMsg);
+};
 
-// We'll dive deeper into Conditional Types later in the course
+// If we use the parent type for an annotation, like object to annotate an array, TS won't complain because object is the parent
+let strings: Object = ["a", "b"];
+let myFunc: Function = () => 2;
