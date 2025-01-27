72c43776b19f719a3ad8dc195dcbcae2026eca42
Imanol <idelaiglesia.m@gmail.com>
2025-01-27 18:17:08 +0100

    33. Nested Objects

--- index.ts
+++ index.ts
@@ -1,19 +1,36 @@
-// We can use Type declarations using Type Alias
-type Post = {
+// One of having nested objects
+// type Post = {
+//   title: string; // Type Annotations using semi-colons instead of commas
+//   content: string;
+//   date: Date;
+//   author: {
+//     name: string;
+//     age: number;
+//     email: string;
+//   };
+// };
+
+// Better to declare a separate type for Author
+type Author = {
+    name: string;
+    age: number;
+    email: string;
+  };
+  
+  type Post = {
     title: string; // Type Annotations using semi-colons instead of commas
     content: string;
     date: Date;
+    author: Author; // Assign Author type to author property on Post type
   };
   
   let post: Post = {
     title: "This is a blog post",
     content: "Content of the post",
     date: new Date(),
-  };
-  
-  // Advantage of type declaration, can be recycled
-  let post2: Post = {
-    title: "This is a blog post 2",
-    content: "Content of the post 2",
-    date: new Date(),
+    author: {
+      name: "John",
+      age: 32,
+      email: "john@doe.com",
+    },
   };
