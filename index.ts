672d56651b1bcc4040a63d2a7b61f00791a582de
Imanol <idelaiglesia.m@gmail.com>
2025-05-12 19:21:41 +0200

    Exercise 10

--- index.ts
+++ index.ts
@@ -1,15 +1,25 @@
-// At times there are functions which do not return anything
-// this function does not return anything
-// Hence for rerturn type we can use a special TypeScript type called void
-function writeToDatabase(value: string): void {
-  console.log("Writing to database:", value);
+type FetchDataFunction = (URL: string, ...queries: string[]) => Promise<string[]>
+
+const fetchData: FetchDataFunction = async (URL, ...queries) => {
+  const fullQuery = URL + "?" + queries.join("&");
+
+  const response = await fetch(fullQuery);
+  const data = await response.json();
+  return data;
 }
 
-// This is different from void because this function never completes execution
-function throwError(error: string): never {
-  throw new Error(error);
+type User = {
+  firstName: string;
+  lastName: string;
+  age: number;
 }
 
-// We check these types and hence void can be used in place of never but not visa versa
-type check = never extends void ? true : false;
-type checks = void extends never ? true : false;
+const user: User = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30
+}
+
+function getUserInfo({ firstName, lastName, age }: User) {
+  return `User: ${firstName} ${lastName}, Age: ${age}`
+}
